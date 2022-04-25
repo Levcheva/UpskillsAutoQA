4233c132bb9b24be991eeea0a4d6bd78c702fa13
Kam Levchev <kameliya.levcheva@mentormate.com>
2022-04-25 17:25:23 +0300

    Lecture 6, task 3, c)

--- src/com/lecture6/methods/L6Task3.java
+++ src/com/lecture6/methods/L6Task3.java
@@ -13,6 +13,25 @@ public class L6Task3 {
         else {
             System.out.println(myString.charAt(length / 2));
         }
-   }
 
+        String stringCount = "Please, count the words of this string if you can!";
+        count_Words(stringCount);
+   }
+    public static int count_Words(String str)
+    {
+        int count = 0;
+        if (!(" ".equals(str.substring(0, 1))) || !(" ".equals(str.substring(str.length() - 1))))
+        {
+            for (int i = 0; i < str.length(); i++)
+            {
+                if (str.charAt(i) == ' ')
+                {
+                    count++;
+                }
+            }
+            count = count + 1;
+        }
+        System.out.println("The number of the words in the string is: " + count);
+        return count; // returns 0 if string starts or ends with space " ".
+    }
 }
