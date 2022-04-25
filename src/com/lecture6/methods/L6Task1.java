55d76a3ce3854db2970cbbc60ede605a0eca95f8
Kam Levchev <kameliya.levcheva@mentormate.com>
2022-04-25 17:14:06 +0300

    HW 6, Task 3

--- src/com/lecture6/methods/L6Task1.java
+++ src/com/lecture6/methods/L6Task1.java
@@ -1,7 +1,5 @@
 package com.lecture6.methods;
 
-import java.util.Scanner;
-
 public class L6Task1 {
     public static void main(String[] args) {
         double x = 4;
--- src/com/lecture6/methods/L6Task3.java
+++ src/com/lecture6/methods/L6Task3.java
@@ -2,6 +2,17 @@ package com.lecture6.methods;
 
 public class L6Task3 {
     public static void main(String[] args) {
+        String myString = "12345678";
+
+        int length = myString.length();
+
+        if (length % 2 == 0) {
+            System.out.println(myString.charAt(length / 2));
+            System.out.println(myString.charAt((length / 2) - 1));
+        }
+        else {
+            System.out.println(myString.charAt(length / 2));
+        }
    }
 
 }
